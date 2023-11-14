456f67f1dde368cbf4ba6584b5dfb69252e9241f
Cosme <cosmequino@gmail.com>
2023-11-14 16:45:21 -0500

    implementacion reportes a DTO
    implementacion de reportes en DTO
    MesMayorDeuda
    MesDeuda

---
+++ src/main/java/pe/edu/upc/aaw/behappycommunity/dtos/MesDeudaReDTO.java
@@ -0,0 +1,23 @@
+package pe.edu.upc.aaw.behappycommunity.dtos;
+
+public class MesDeudaReDTO {
+    private String  mes;
+    private double total;
+
+
+    public String getMes() {
+        return mes;
+    }
+
+    public void setMes(String mes) {
+        this.mes = mes;
+    }
+
+    public double getTotal() {
+        return total;
+    }
+
+    public void setTotal(double total) {
+        this.total = total;
+    }
+}
